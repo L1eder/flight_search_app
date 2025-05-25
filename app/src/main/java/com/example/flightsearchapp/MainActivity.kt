4af78cb7c068dfb2fbf186b4fbde0f785f71b936
Lydia <lida_zhuikova@mail.ru>
2025-05-26 01:37:20 +0400

    feat: add basic display functionality to MainActivity

--- app/src/main/java/com/example/flightsearchapp/MainActivity.kt
+++ app/src/main/java/com/example/flightsearchapp/MainActivity.kt
@@ -1,47 +1,38 @@
 package com.example.flightsearchapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.flightsearchapp.ui.theme.FlightSearchAppTheme
+import com.example.flightsearchapp.data.repository.FlightRepository
+import com.example.flightsearchapp.preferences.PreferencesManager
+import com.example.flightsearchapp.ui.viewmodel.FlightViewModel
+import com.example.flightsearchapp.ui.viewmodel.FlightViewModelFactory
+import androidx.datastore.preferences.preferencesDataStore
+import com.example.flightsearchapp.data.database.DatabaseProvider
+import com.example.flightsearchapp.ui.screens.FlightSearchScreen
+
+val ComponentActivity.dataStore by preferencesDataStore(name = "settings")
 
 class MainActivity : ComponentActivity() {
+    private lateinit var viewModel: FlightViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        val db = DatabaseProvider.getDatabase(this)
+
+        val repository = FlightRepository(db.airportDao(), db.favoriteDao())
+
+        val preferencesManager = PreferencesManager(dataStore)
+
+        val viewModelFactory = FlightViewModelFactory(repository, preferencesManager)
+
+        Log.d("ViewModel", "Initializing ViewModel...")
+        viewModel = viewModelFactory.create(FlightViewModel::class.java)
+
         setContent {
-            FlightSearchAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            FlightSearchScreen(viewModel)
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FlightSearchAppTheme {
-        Greeting("Android")
     }
 }
